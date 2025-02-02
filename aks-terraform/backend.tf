aa394297b0757a1846410c0767ddf420d67d7b31
Bavithran M <79276699+bavicnative@users.noreply.github.com>
2025-02-02 22:17:08 +0530

    Update backend.tf

--- aks-terraform/backend.tf
+++ aks-terraform/backend.tf
@@ -1,1 +1,9 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name   = "tfstate-rg"
+    storage_account_name  = "tfstateaksstorage"
+    container_name        = "tfstate"
+    key                   = "terraform.tfstate"
+  }
+}
 
